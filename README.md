ff17e1da556fc3397fc63261cf72a8f1a4ad7b78
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-27 22:13:32 +0530

    accordion

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Bootstrap Components 
 
 made using Css(flexbox for layout) and Jquery only.
+
+# List of Components
+
+1. Collapse component - accordion format
+2. Navbar component
+3. Modal component
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,50 @@
-$(document).ready(()=>{
 
-	
+const refreshComponent = () => {
+	$('.accordion-container').hide();
+    $('.navbar-container').hide();
+    $('.modal-container').hide();
+
+    $('.multiscroll-nav li').css("color","#555");
+};
+
+const componentNavigate = e => {
+
+    const componentType = e.target.closest('li').dataset.type;
+
+    if (componentType) {
+        
+        refreshComponent();
+
+        if (componentType === "accord") {
+            $('.accordion-container').show(200);
+            $('.multiscroll-nav li').eq(0).css("color","#007BFF");
+        } else if (componentType === "nav") {
+            $('.navbar-container').show(200);
+            $('.multiscroll-nav li').eq(1).css("color","#007BFF");
+        } else if (componentType === "mod") {
+            $('.modal-container').show(200);
+            $('.multiscroll-nav li').eq(2).css("color","#007BFF");
+        }
+    }
+};
+
+$('.sidebar .list').on('click', componentNavigate);
+$('.multiscroll-nav ul').on('click', componentNavigate);
+
+const refreshAccordion = () => {
+
+};
+
+const accordionAnimate = e => {
+	refreshAccordion();
+};
+
+$('.card-header').on('click',accordionAnimate);
+
+$(document).ready(() => {
+
+    $('.navbar-container').hide();
+    $('.modal-container').hide();
+    $('.card-body').eq(0).show(500);
+
 });
